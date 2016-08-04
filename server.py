861d3316380abee8aa556b92cfc6ecf6c15a5090
Hackbright Student <info@hackbrightacademy.com>
2016-08-03 18:04:09 -0700

    finished checking user name and password and redirecting appropriately

--- server.py
+++ server.py
@@ -49,24 +49,62 @@ def register_process():
     """Confirm user registered."""
 
     #This is Flask formatting for grabbing elements from a form/form handling in general
-    username = request.form.get('username')
+    email = request.form.get('email')
     password = request.form.get('password')
+    age = request.form.get('age')
+    zipcode = request.form.get('zipcode')
 
-    #SQLAlchemy
-    is_user = User.query.filter_by(user_id='username').first() #querying db for username match
+    #SQLAlchemy - querying db for username match
+    is_user = User.query.filter_by(email='email').first()
+
+    #Instantiating new_user
+    new_user = User(email, password, age, zipcode)
 
     if is_user is not None: #if user exists
+        session['user_has_been_added']= is_user.email
+        flash('Your account already exists. We\'ve logged you in!')
         return redirect('/') #redirect to homepage
     else: #come back to this in afternoon
-        db.session.add(#something) #write new user to db
+        db.session.add(new_user) #write new user to db
         db.session.commit()
+        session['user_has_been_added']= new_user.email
+        flash("Welcome! Your account has been created")
         return redirect('/') #redirect to homepage
 
+    #this is where user verification happens
+    #add user to db.session, commit changes
+    #flash if user exists (some msg)
+    #flash if new user (some msg)
 
 
+@app.route("/login", methods=["GET"])
+def login_form():
+    """Show login form."""
 
- #this is where user verification happens
- #add user to db.session, commit changes
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site.
+
+    Find the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session.
+    """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    #SQLAlchemy - querying db for email and password match
+    is_user = User.query.filter((email=='email') & (password=='password')).first()
+
+    if is_user is not None:
+        session['user_has_been_added']= is_user.email
+        flash('You\'ve been successfully logged in!')
+        return redirect('/')
+    else:
+        flash('Sorry, we could not find you. Please register.')
+        return redirect('/register')
 
 
 if __name__ == "__main__":
